1b06ee72c4e6f62662ca656442c833c3d9fcf0ff
Skygraphy <44362256+Skygraphy@users.noreply.github.com>
2021-03-09 17:10:10 +0100

    pype

--- server.js
+++ server.js
@@ -1,13 +1,47 @@
+const env = require('./env');
+const path = require('path');
+
 const express = require('express');
 const app = express();
-const port = 1337;
+const http = require('http').Server(app);
+
+const ParseServer = require('parse-server').ParseServer;
+const ParseDashboard = require('parse-dashboard');
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+const parseServer = new ParseServer({
+
+    appId: env.parseServer.appId,
+    appName: env.parseServer.appName,
+    cloud: env.parseServer.cloud,
+    databaseURI: env.parseServer.databaseURI,
+    logLevel: env.parseServer.logLevel,
+    masterKey: env.parseServer.masterKey,
+    maxUploadSize: env.parseServer.maxUploadSize,
+    publicServerURL: env.parseServer.publicServerURL,
+    serverURL: env.parseServer.serverURL,
+    verifyUserEmails: env.parseServer.verifyUserEmails
+});
+
+app.use(env.parseServer.mountPath, parseServer);
 
 app.get('/', (req, res) => {
 
-    res.send('Hello World!');
-})
+    res.status(200).send(env.parseServer.appName);
+});
 
-app.listen(port, () => {
+http.listen(env.port, () => {
 
-    console.log(`pype-server listening at http://localhost:${port}`);
-})
+    console.log(env.parseServer.appName + ' running on port ' + env.port);
+});
+
+// app.get('/', (req, res) => {
+
+//     res.send('Hello World!');
+// })
+
+// app.listen(port, () => {
+
+//     console.log(`pype-server listening at http://localhost:${port}`);
+// })
