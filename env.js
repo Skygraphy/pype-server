1989a8f465a04cb98315605f9326540fe1be5392
Skygraphy <44362256+Skygraphy@users.noreply.github.com>
2021-03-16 09:01:27 +0100

    pype

--- env.js
+++ env.js
@@ -9,10 +9,10 @@ const env = {
         appId: process.env.PARSE_SERVER_APP_ID || '71SliqYnrwVFOc1NHKgQl7tXJ7m1oCYtJx66CbvGjStdQxACBiNOBPqL4NHc0WYLsewYhiHz26wX20S9',
         appName: process.env.PARSE_SERVER_APP_NAME || 'pype-server',
         cloud: process.env.PARSE_SERVER_CLOUD || path.join(__dirname, '/cloud/main.js'),
-        dashboardPassword: process.env.PARSE_SERVER_DASHBOARD_PASSWORD || '$2y$12$eo5ub5QnplinTqhcRC4Af.O30ho5mVUdFlG00Jax1vk3h9sdtVfqm',
+        dashboardPassword: process.env.PARSE_SERVER_DASHBOARD_PASSWORD || '$2y$12$eo5ub5QnplinTqhcRC4Af.O30ho5mVUdFlG00Jax1vk3h9sdtVfqm', // Explorer01
         dashboardPath: process.env.PARSE_SERVER_DASHBOARD_PATH || '/dashboard',
         dashboardUser: process.env.PARSE_SERVER_DASHBOARD_USER || 'pype-server',
-        databaseURI: process.env.PARSE_SERVER_DATABASE_URI || 'mongodb://localhost:27017/dev',
+        databaseURI: process.env.PARSE_SERVER_DATABASE_URI || 'mongodb://localhost:27018/dev',
         filesUrl: process.env.PARSE_SERVER_FILES_URL || 'http://localhost:1337/parse/files',
         logLevel: process.env.PARSE_SERVER_LOG_LEVEL || 'error',
         masterKey: process.env.PARSE_SERVER_MASTER_KEY || 'BFjfZArrmkDMJMKUPSKoXuGy7FALWnoRueQ0uwtDJ5kwyaEdS6pfnoHoC9l83Qd9lmtt7Qmv4zTM43Fa',
@@ -20,7 +20,7 @@ const env = {
         mountPath: process.env.PARSE_SERVER_MOUNT_PATH || '/parse',
         publicServerURL: process.env.PARSE_SERVER_PUBLIC_SERVER_URL || 'http://localhost:1337/parse',
         serverURL: process.env.PARSE_SERVER_SERVER_URL || 'http://localhost:1337/parse',
-        verifyUserEmails: false
+        verifyUserEmails: process.env.PARSE_SERVER_VERIFY_USER_EMAILS || false
     }
 };
 
--- server.js
+++ server.js
@@ -12,36 +12,54 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 const parseServer = new ParseServer({
 
-    appId: env.parseServer.appId,
-    appName: env.parseServer.appName,
-    cloud: env.parseServer.cloud,
-    databaseURI: env.parseServer.databaseURI,
-    logLevel: env.parseServer.logLevel,
-    masterKey: env.parseServer.masterKey,
-    maxUploadSize: env.parseServer.maxUploadSize,
-    publicServerURL: env.parseServer.publicServerURL,
-    serverURL: env.parseServer.serverURL,
-    verifyUserEmails: env.parseServer.verifyUserEmails
+  appId: env.parseServer.appId,
+  appName: env.parseServer.appName,
+  cloud: env.parseServer.cloud,
+  databaseURI: env.parseServer.databaseURI,
+  logLevel: env.parseServer.logLevel,
+  masterKey: env.parseServer.masterKey,
+  maxUploadSize: env.parseServer.maxUploadSize,
+  publicServerURL: env.parseServer.publicServerURL,
+  serverURL: env.parseServer.serverURL,
+  verifyUserEmails: env.parseServer.verifyUserEmails
 });
 
 app.use(env.parseServer.mountPath, parseServer);
 
+const parseDashboard = new ParseDashboard({
+
+  apps: [
+    {
+      appId: env.parseServer.appId,
+      appName: env.parseServer.appName,
+      masterKey: env.parseServer.masterKey,
+      production: true,
+      serverURL: env.parseServer.serverURL
+    }
+  ],
+  trustProxy: 1,
+  useEncryptedPasswords: true,
+  users: [
+    {
+      pass: env.parseServer.dashboardPassword,
+      user: env.parseServer.dashboardUser
+    }
+  ]
+}, { allowInsecureHTTP: true, cookieSessionSecret: env.parseServer.masterKey });
+
+app.use(env.parseServer.dashboardPath, parseDashboard);
+
 app.get('/', (req, res) => {
 
-    res.status(200).send(env.parseServer.appName);
+  res.status(200).send(env.parseServer.appName);
+});
+
+app.use((err, req, res, next) => {
+
+  res.status(500).json({ message: err.message });
 });
 
 http.listen(env.port, () => {
 
-    console.log(env.parseServer.appName + ' running on port ' + env.port);
+  console.log(env.parseServer.appName + ' running on port ' + env.port);
 });
-
-// app.get('/', (req, res) => {
-
-//     res.send('Hello World!');
-// })
-
-// app.listen(port, () => {
-
-//     console.log(`pype-server listening at http://localhost:${port}`);
-// })
